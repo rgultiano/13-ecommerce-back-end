f813f56dcd9959bbfd8db875259d7f340e877ca8
Ron G <62861315+rgultiano@users.noreply.github.com>
2021-08-23 13:20:08 +1000

    defined Product Columns

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,37 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    mpriceodel: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate:{
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate:{
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
